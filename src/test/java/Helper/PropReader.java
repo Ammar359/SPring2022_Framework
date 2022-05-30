5b6d95cfcef8a384d50812044143252676af32d5
Ammar Ashraf <umaar4u@hotmail.com>
2022-05-30 15:41:04 -0400

    additions

---
+++ src/test/java/Helper/PropReader.java
@@ -0,0 +1,55 @@
+package Helper;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropReader {
+
+    private String url = "";
+    private String browser = "";
+    private String version = "";
+    private String sauceUser = "";
+    private String sauceKey = "";
+    private String os = "";
+    private String runOn = "";
+
+    public PropReader() {
+        Properties prop = new Properties();
+        try {
+            InputStream is = new FileInputStream("src/test/resources/Data.properties");
+
+            prop.load(is);
+
+            url = prop.getProperty("url");
+            browser = prop.getProperty("browser");
+            version = prop.getProperty("version");
+            runOn = prop.getProperty("runOn");
+            sauceUser = prop.getProperty("sauceUser");
+            sauceKey = prop.getProperty("sauceKey");
+            os = prop.getProperty("os");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getSauceUser() {return sauceUser;}
+
+    public String getSauceKey() {return sauceKey;}
+
+    public String getOs() {return os;}
+
+    public String getBrowser() {return browser;}
+    public String getVersion() {return version;}
+
+    public String getRunOn() {return runOn;}
+
+
+
+
+}
